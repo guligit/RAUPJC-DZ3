8246883df99d71a619ae94b68ba3d23033f60c34
guligit <guligit@users.noreply.github.com>
2016-12-12 12:48:25 +0100

    add whole progra,m

---
+++ Models/TodoItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ToDoList.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? DateCompleted { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Guid UserId { get; set; }
+
+        public TodoItem(string text, Guid userId)
+        {
+            Id = Guid.NewGuid();
+            Text = text;
+            IsCompleted = false;
+            DateCreated = DateTime.Today;
+            UserId = userId;
+        }
+
+        public TodoItem()
+        {
+            
+        }
+
+        public void MarkAsCompleted()
+        {
+            if (!IsCompleted)
+            {
+                IsCompleted = true;
+                DateCompleted = DateTime.Today;
+            }
+        }
+    }
+}
